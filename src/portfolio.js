d01ab722a41d67ed29b62fe6502b786e6de65e6a
Kremzon <Nadavbeker@gmail.com>
2022-03-30 16:02:11 +0300

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -21,7 +21,7 @@ const illustration = {
 
 const greeting = {
   username: "Nadav Kremer",
-  title: "Hi all, I'm Nadav",
+  title: "Hi all, I'm Nadav!",
   subTitle: emoji(
     "A passionate Full Stack Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks."
   ),
