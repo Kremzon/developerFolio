55ac72ad0b0633c5d6dae0c7590f964dd390e724
Kremzon <Nadavbeker@gmail.com>
2022-03-30 13:19:42 +0000

    Deploying to gh-pages from  @ 6c264d9cea1345f515f3b0660712d7877e5b0d3d 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/developerFolio/precache-manifest.2c0979bddafdc7033e97d29948d6412d.js"
+  "/precache-manifest.dfcab299193043404d633411f0f374eb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/developerFolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
